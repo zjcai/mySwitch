9a40e8622e1299284ac14c2b8e826cfcb1efa704
acai-toaster <angela.cai@toasttab.com>
2020-04-09 12:20:42 -0400

    cleanup

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const fs = require('fs');
 
 const wgetC = 'wget -O result.html https://www.bestbuy.com/site/nintendo-switch-32gb-console-neon-red-neon-blue-joy-con/6364255.p?skuId=6364255'
 
-child_process.exec(wgetC, (error, stdout) => {
-  if (error) {
-    console.log(error);
-  }
-  console.log("2 " + stdout);
-  checkAvailability()
-});
+while (true) {
+  child_process.exec(wgetC, (error, stdout) => {
+    if (error) {
+      console.log(error);
+    }
+    checkAvailability()
+  });
+
+  await sleep(2000)
+
+}
 
 function checkAvailability() {
   const $ = cheerio.load(fs.readFileSync('result.html')); //use as JQuery
