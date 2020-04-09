e618ddcd94fb9ca5cdd234c977752bc1b8c5bdd6
Zijing Cai <angelacai@angela-mbp.local>
2020-04-09 17:50:35 -0400

    change from wget to curl bucause mac development tool does not have wget

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const cheerio = require("cheerio");
 const child_process = require("child_process");
 const fs = require('fs');
 
-const wgetC = 'wget -O result.html https://www.bestbuy.com/site/nintendo-switch-32gb-console-neon-red-neon-blue-joy-con/6364255.p?skuId=6364255'
+// wget works but not curl before because missing the user-agent for curl
+const downloadFromUrl = 'curl -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36" https://www.bestbuy.com/site/nintendo-switch-32gb-console-neon-red-neon-blue-joy-con/6364255.p?skuId=6364255 -o result.html'
 
 startProcess();
 setInterval(() => {  startProcess(); }, 1000000);
   
 function startProcess() {
-  child_process.exec(wgetC, (error, stdout) => {
+  child_process.exec(downloadFromUrl, (error, stdout) => {
     if (error) {
       console.log(error);
     }
