58a3fa2e1a8810858ad9eeab2d1de3d5ad3cec8b
acai-toaster <angela.cai@toasttab.com>
2020-04-09 12:57:30 -0400

    add interval for run in every 10 mins

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ const fs = require('fs');
 
 const wgetC = 'wget -O result.html https://www.bestbuy.com/site/nintendo-switch-32gb-console-neon-red-neon-blue-joy-con/6364255.p?skuId=6364255'
 
-while (true) {
+startProcess();
+setInterval(() => {  startProcess(); }, 1000000);
+  
+function startProcess() {
   child_process.exec(wgetC, (error, stdout) => {
     if (error) {
       console.log(error);
     }
-    checkAvailability()
+    checkAvailability();
   });
-
-  await sleep(2000)
-
 }
 
 function checkAvailability() {
   const $ = cheerio.load(fs.readFileSync('result.html')); //use as JQuery
   var buttonText = $('.fulfillment-add-to-cart-button div button').text();
-  console.log(buttonText + ' time: ' + new Date() + '\n')
+  console.log(buttonText + ' time: ' + new Date())
   if (buttonText != 'Sold Out' && buttonText != 'Check Stores') {
     console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
     console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n')
